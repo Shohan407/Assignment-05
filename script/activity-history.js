a89fc2eb382903ac1b49629e3a3054c7a77c8ca3
Shohan407 <xrshohan07@gmail.com>
2025-03-01 23:38:52 +0600

    added js date

--- script/activity-history.js
+++ script/activity-history.js
@@ -8,8 +8,25 @@ document.getElementById('clear-history').addEventListener('click',
     });
 
 
+// background color change section
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
+let index = 0;
 
- 
+document.getElementById('background-color').addEventListener('click',
+    function () {
+        document.body.style.backgroundColor = colors[index];
+        index = (index + 1) % colors.length;
+    });
+
+// current date section
+document.addEventListener("DOMContentLoaded", function(){
+    const now = new Date();
+    const weekday = now.toLocaleDateString("en-US", { weekday: "short" });
+    const currentDate = now.toLocaleDateString("en-US", {
+        month: "short",day: "numeric",year: "numeric"
+      });
+      document.getElementById("current-Date").innerHTML = `${weekday}<br>${currentDate}`;
+});
 
 // 1st active history js
 const outputHeading = document.getElementById('active-text');
@@ -24,12 +41,12 @@ document.getElementById('card-btn').addEventListener('click',
         // task count section
         const taskCount = document.getElementById('change-date').innerText;
         const convertTaskCount = parseFloat(taskCount);
-        sum = convertTaskCount - 1 ;
+        sum = convertTaskCount - 1;
         document.getElementById('change-date').innerText = sum;
         // navCount section
         const navCount = document.getElementById('change-number').innerText;
         const convertnavCount = parseFloat(navCount);
-        navSum = convertnavCount + 1 ;
+        navSum = convertnavCount + 1;
         document.getElementById('change-number').innerText = navSum;
 
         this.setAttribute("disabled", true);
@@ -52,12 +69,12 @@ document.getElementById('card-btn2').addEventListener('click',
         // task count section
         const taskCount = document.getElementById('change-date').innerText;
         const convertTaskCount = parseFloat(taskCount);
-        sum = convertTaskCount - 1 ;
+        sum = convertTaskCount - 1;
         document.getElementById('change-date').innerText = sum;
         // navCount section
         const navCount = document.getElementById('change-number').innerText;
         const convertnavCount = parseFloat(navCount);
-        navSum = convertnavCount + 1 ;
+        navSum = convertnavCount + 1;
         document.getElementById('change-number').innerText = navSum;
 
         this.setAttribute("disabled", true);
@@ -79,12 +96,12 @@ document.getElementById('card-btn3').addEventListener('click',
         // task count section
         const taskCount = document.getElementById('change-date').innerText;
         const convertTaskCount = parseFloat(taskCount);
-        sum = convertTaskCount - 1 ;
+        sum = convertTaskCount - 1;
         document.getElementById('change-date').innerText = sum;
         // navCount section
         const navCount = document.getElementById('change-number').innerText;
         const convertnavCount = parseFloat(navCount);
-        navSum = convertnavCount + 1 ;
+        navSum = convertnavCount + 1;
         document.getElementById('change-number').innerText = navSum;
 
         this.setAttribute("disabled", true);
@@ -106,12 +123,12 @@ document.getElementById('card-btn4').addEventListener('click',
         // task count section
         const taskCount = document.getElementById('change-date').innerText;
         const convertTaskCount = parseFloat(taskCount);
-        sum = convertTaskCount - 1 ;
+        sum = convertTaskCount - 1;
         document.getElementById('change-date').innerText = sum;
         // navCount section
         const navCount = document.getElementById('change-number').innerText;
         const convertnavCount = parseFloat(navCount);
-        navSum = convertnavCount + 1 ;
+        navSum = convertnavCount + 1;
         document.getElementById('change-number').innerText = navSum;
 
         this.setAttribute("disabled", true);
@@ -133,12 +150,12 @@ document.getElementById('card-btn5').addEventListener('click',
         // task count section
         const taskCount = document.getElementById('change-date').innerText;
         const convertTaskCount = parseFloat(taskCount);
-        sum = convertTaskCount - 1 ;
+        sum = convertTaskCount - 1;
         document.getElementById('change-date').innerText = sum;
         // navCount section
         const navCount = document.getElementById('change-number').innerText;
         const convertnavCount = parseFloat(navCount);
-        navSum = convertnavCount + 1 ;
+        navSum = convertnavCount + 1;
         document.getElementById('change-number').innerText = navSum;
 
         this.setAttribute("disabled", true);
@@ -160,12 +177,12 @@ document.getElementById('card-btn6').addEventListener('click',
         // task count section
         const taskCount = document.getElementById('change-date').innerText;
         const convertTaskCount = parseFloat(taskCount);
-        sum = convertTaskCount - 1 ;
+        sum = convertTaskCount - 1;
         document.getElementById('change-date').innerText = sum;
         // navCount section
         const navCount = document.getElementById('change-number').innerText;
         const convertnavCount = parseFloat(navCount);
-        navSum = convertnavCount + 1 ;
+        navSum = convertnavCount + 1;
         document.getElementById('change-number').innerText = navSum;
 
         this.setAttribute("disabled", true);

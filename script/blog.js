a4b66c526d77f3f85be009d9c495d6390f2932e1
Shohan407 <xrshohan07@gmail.com>
2025-03-02 20:52:44 +0600

    github problems sove

--- script/blog.js
+++ script/blog.js
@@ -1,6 +1,6 @@
 document.getElementById('blog-btn').addEventListener('click',
     function (event) {
         event.preventDefault();
-        window.location.href = "../Assignment-05/blog.html"
+        window.location.href = "../blog.html"
     }
 )
